45c1376c8ddc05a567e328ee252b11034c319926
Ben Hulan <benhulan@gmail.com>
2016-02-29 14:48:14 -0800

    in-class;

--- app.js
+++ app.js
@@ -4,6 +4,29 @@ var port = process.env.PORT || 3000;
 var router = express.Router();
 var bodyParser = require('body-parser');
 
+var candies = [{
+    id: 16,
+    name: "Chewing Gum",
+    color: "Red"
+},
+{
+    id: 20, 
+    name: "Pez",
+    color: "Green"
+},
+{
+    id: 304,
+    name: "Marshmallow",
+    color: "Pink"
+},
+{
+    id: 404,
+    name: "Candy Stick",
+    color: "Blue"
+}];
+
+var candy = {};
+
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
@@ -25,6 +48,26 @@ router.get('/contact', function(req, res){
 
 router.get('/about', function(req, res){
     res.render('about', {header: 'about!'});
+});
+
+router.get('/candies', function(req, res){
+    res.json(candies);
+});
+
+router.post('/candies', function(req, res){
+    console.log(req);
+    // var candy_id = req.params.id;
+    // candies.push(candy_id);
+    res.json(candies);
+});
+
+router.get('/candies/:id', function(req, res){
+    for (var i = 0; i < candies.length; i++){
+        if ( req.params.id == candies[i].id){
+            candy = candies[i];
+            res.json(candy);
+        }
+    }
 });
 
 app.use('/', router);
